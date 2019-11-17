331eaf991c0bdde314df59eeeea46d7c8a6725fd
wangxingkang <156148958@qq.com>
2019-11-17 22:58:21 +0800

    feat: update pansy-utils

--- packages/pansy-utils/src/index.ts
+++ packages/pansy-utils/src/index.ts
@@ -1,7 +1,14 @@
 // is
-export { default as isNull } from '@pansy/is-null';
 export { default as isArray } from '@pansy/is-array';
+export { default as isBoolean } from '@pansy/is-boolean';
 export { default as isFunction } from '@pansy/is-function';
+export { default as isNil } from '@pansy/is-nil';
+export { default as isNull } from '@pansy/is-null';
+export { default as isString } from '@pansy/is-string';
+export { default as isUndefined } from '@pansy/is-undefined';
 
 export { default as indent } from '@pansy/indent';
 export { default as dent } from '@pansy/dent';
+
+// 类方法
+export { default as Policy } from '@pansy/policy';
