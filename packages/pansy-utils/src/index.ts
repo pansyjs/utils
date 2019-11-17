ef6e36dffb53c9151cd4bfe232f641b38ab36bf2
wangxingkang <156148958@qq.com>
2019-11-17 23:37:04 +0800

    feat: update pansy-utils

--- packages/pansy-utils/src/index.ts
+++ packages/pansy-utils/src/index.ts
@@ -5,10 +5,15 @@ export { default as isFunction } from '@pansy/is-function';
 export { default as isNil } from '@pansy/is-nil';
 export { default as isNull } from '@pansy/is-null';
 export { default as isString } from '@pansy/is-string';
+export { default as isDate } from '@pansy/is-date';
 export { default as isUndefined } from '@pansy/is-undefined';
 
 export { default as indent } from '@pansy/indent';
 export { default as dent } from '@pansy/dent';
+export { default as getType } from '@pansy/get-type';
 
 // 类方法
 export { default as Policy } from '@pansy/policy';
+
+// 一些类型定义
+export * from '@pansy/enhance-type';
