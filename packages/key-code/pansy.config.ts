df65d8c1c2ee177e74b0663179b1d90e0bf7b8d3
wangxingkang <156148958@qq.com>
2019-12-10 15:03:43 +0800

    feat: add @pansy/key-code

---
+++ packages/key-code/pansy.config.ts
@@ -0,0 +1,10 @@
+import { Config } from '@walrus/pansy';
+
+const config: Config = {
+  output: {
+    format: ['cjs', 'es', 'umd', 'umd-min'],
+    moduleName: 'keyCode'
+  }
+};
+
+export default config;
