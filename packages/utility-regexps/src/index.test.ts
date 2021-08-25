78fe09fa3853d17c86ee6eb8c5c5182989847bf1
wangxingkang <wang_xingkang@qq.com>
2021-08-25 17:05:36 +0800

    feat(utility-regexps): add idCardRegexp

---
+++ packages/utility-regexps/src/index.test.ts
@@ -0,0 +1,20 @@
+import { idCardRegexp } from './';
+
+describe('idCardRegexp', () => {
+  test('合格的身份证号', () => {
+    expect(idCardRegexp.test('142701199309095656')).toEqual(true);
+  });
+
+  test('合格的身份证号，最后一位x', () => {
+    expect(idCardRegexp.test('14270119930909565X')).toEqual(true);
+    expect(idCardRegexp.test('14270119930909565x')).toEqual(true);
+  });
+
+  test('存在非法字符', () => {
+    expect(idCardRegexp.test('1427011993090a565X')).toEqual(false);
+  });
+
+  test('字符非18位', () => {
+    expect(idCardRegexp.test('14270119')).toEqual(false);
+  });
+});
--- packages/utility-regexps/src/index.ts
+++ packages/utility-regexps/src/index.ts
@@ -12,3 +12,9 @@ export const emailRegexp = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
  * url正则
  */
 export const urlRegexp = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+
+// 身份证号
+export const idCardRegexp = new RegExp(
+  /^[1-9]\d{5}(18|19|20|(3\d))\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/
+);
+
