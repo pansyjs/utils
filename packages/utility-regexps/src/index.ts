1dc17b6d1a1e266eae80c814ac800f4c2fada012
wangxingkang <156148958@qq.com>
2021-12-27 11:43:45 +0800

    ✨ feat(utility-regexps): 身份证号正则方便提取性别

--- packages/utility-regexps/src/index.ts
+++ packages/utility-regexps/src/index.ts
@@ -14,5 +14,5 @@ export const emailRegexp = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
 export const urlRegexp = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
 
 // 身份证号
-export const idCardRegexp = /^[1-9]\d{5}(\d{4})(0[1-9]|1[0-2])([0-2][1-9]|10|20|30|31)\d{3}[0-9Xx]$/;
+export const idCardRegexp = /^[1-9]\d{5}(\d{4})(0[1-9]|1[0-2])([0-2][1-9]|10|20|30|31)\d{2}(\d)[0-9Xx]$/;
 
